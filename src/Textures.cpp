a255021650f62c96f6954b0130fd32d9b3aa8ce9
Qwertee <mobiusdragon96@gmail.com>
2018-08-19 16:00:15 -0500

    updated to copy dependant libraries to output folder on Windows.

--- src/Textures.cpp
+++ src/Textures.cpp
@@ -1,28 +1,14 @@
 #include "Textures.hpp"
 
-// for changing the working directory for Windows compatibility
-#if _WIN32
-#include <direct.h>
-#endif //_WIN32
-
 sf::Texture Textures::grass;
 sf::Texture Textures::character;
 sf::Texture Textures::water;
 sf::Texture Textures::mountain;
 
 void Textures::init() {
-
-  /* need to change the directory path for Windows because it likes to screw
-   * with file paths (executables are put an additional directory deep from what
-   * linux does, so cd one directory up for the paths to work again
-   */
-#if _WIN32
-  _chdir("../");
-#endif // _WIN32
-
   // load the files
-  grass.loadFromFile("../Assets/Grass.png");
-  character.loadFromFile("../Assets/Character.png");
-  water.loadFromFile("../Assets/Water.png");
-  mountain.loadFromFile("../Assets/Mountain.png");
+  grass.loadFromFile("Assets/Grass.png");
+  character.loadFromFile("Assets/Character.png");
+  water.loadFromFile("Assets/Water.png");
+  mountain.loadFromFile("Assets/Mountain.png");
 }
